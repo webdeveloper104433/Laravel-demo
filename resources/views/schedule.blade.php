e9a05e40e686ebe119084e75f72e5eb0ccb124bc
webdeveloper104433 <web.developer104433@gmail.com>
2021-08-04 14:01:04 +0900

    feat: fix some files

--- resources/views/schedule.blade.php
+++ resources/views/schedule.blade.php
@@ -20,14 +20,14 @@
       border-collapse: separate;
       box-sizing: border-box;
       text-indent: initial;
-      border-spacing: 20px;
+      border-spacing: 10px;
       border-color: grey;
       border: 1px solid grey;
-      border-radius: 10px;
+      border-radius: 20px;
     }
 
     .schedule-border {
-      border: 1px solid grey;
+      border: 1px solid black;
       padding: 10px;
     }
 
@@ -38,29 +38,25 @@
 
   <div class="container">
     <!-- Wrapper for slides -->
-    @if (!(isset($title) && $title != "off"))
-	  @if (isset($schedule_title))
-      <h3>{{ $schedule_title }}</h3>
-	  @endif
-    @endif
+    <h1>Termine</h1>
+
     <div class="table-responsive text-center" style="margin-top: 20px;">
 
-     
-      <table style="width: 100%; ">
+      <table style="width: 100%; " cellspacing="10px">
         <tbody>
           @foreach ($schedules as $schedule)
             @if (($loop->index + 2) % 2 == 0)
               <tr>
             @endif
-                <td class="schedule-border">
+                <td class="schedule-border" style="@if (($loop->index + 2) % 2 == 0) background-color: #0389f7; @else background-color: #e2e2e2; @endif">
                   <div>
                     <img src="{{ asset('storage') . '/' . $schedule->image->url }}" class="img-thumbnail">
                   </div>
-                  <div style="font-size: 25px; margin-top: 10px;">
+                  <div style="font-size: 25px;">
                     <strong>
-                      {{ date('l', strtotime($schedule->date)) }} {{ $schedule->date }}
+                      {{ date('l', strtotime($schedule->date)) }}, {{ $schedule->date }}
                       <br>
-                      {{ $schedule->time }} {{ date('A', strtotime($schedule->date)) }}
+                      Time: {{ $schedule->time }} Clock
                     </strong>
                   </div>
                   <div style="margin-top: 10px;">
